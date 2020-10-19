cfcba0fea9ead6b9a97ffde2548cc71e95bb653a
nullforce <glenngit@nullforce.com>
2020-10-18 20:06:23 -0500

    Update Post model

--- src/PostJson.cs
+++ src/PostJson.cs
@@ -18,16 +18,46 @@ namespace Nullforce.Api.Derpibooru.JsonModels
         public string Author { get; set; }
 
         /// <summary>
+        /// The URL of the author's avatar. May be a link to the CDN path, or a data: URI.
+        /// </summary>
+        [JsonPropertyName("avatar")]
+        public string Avatar { get; set; }
+
+        /// <summary>
         /// The post text.
         /// </summary>
         [JsonPropertyName("body")]
         public string Body { get; set; }
 
         /// <summary>
+        /// The creation time, in UTC, of the comment.
+        /// </summary>
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// The edit reason for this comment, or null if none provided.
+        /// </summary>
+        [JsonPropertyName("edit_reason")]
+        public string EditReason { get; set; }
+
+        /// <summary>
+        /// The time, in UTC, this comment was last edited at, or null if it was not edited.
+        /// </summary>
+        [JsonPropertyName("edited_at")]
+        public DateTime? EditedAt { get; set; }
+
+        /// <summary>
         /// The ID of the topic the post belongs to.
         /// </summary>
         [JsonPropertyName("topic_id")]
         public int? TopicId { get; set; }
+
+        /// <summary>
+        /// The time, in UTC, the comment was last updated at.
+        /// </summary>
+        [JsonPropertyName("updated_at")]
+        public DateTime UpdatedAt { get; set; }
 
         /// <summary>
         /// The ID of the user the comment belongs to, if any.
